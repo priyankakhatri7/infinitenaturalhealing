051be361b07cee88ebecb240d4efbead9793f86f
Priyanka Khatri <priyankakhatri7099@gmail.com>
2023-01-11 17:02:07 +0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.94795d6df45291a56006a9a00968c0a3.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "88427cb90a7240b54df4",
+    "url": "/infinitenaturalhealing/static/js/0.b6e9bc0e.chunk.js"
+  },
+  {
+    "revision": "08e44a4b76080c6e2b0d",
+    "url": "/infinitenaturalhealing/static/js/1.484b7437.chunk.js"
+  },
+  {
+    "revision": "2db7462cdf5043016624",
+    "url": "/infinitenaturalhealing/static/js/main.f4751b01.chunk.js"
+  },
+  {
+    "revision": "73638b36384d958b6706",
+    "url": "/infinitenaturalhealing/static/js/runtime~main.b478480b.js"
+  },
+  {
+    "revision": "d782d3cf6e6adc019753",
+    "url": "/infinitenaturalhealing/static/js/4.ff29529d.chunk.js"
+  },
+  {
+    "revision": "75d8abdb8cef74d0a7cd",
+    "url": "/infinitenaturalhealing/static/js/5.e8420cb8.chunk.js"
+  },
+  {
+    "revision": "ee61d2faea12220a80fe",
+    "url": "/infinitenaturalhealing/static/js/6.3a119876.chunk.js"
+  },
+  {
+    "revision": "fb3aabf37ec4f60899af",
+    "url": "/infinitenaturalhealing/static/css/7.29f8fd8d.chunk.css"
+  },
+  {
+    "revision": "fb3aabf37ec4f60899af",
+    "url": "/infinitenaturalhealing/static/js/7.667fc005.chunk.js"
+  },
+  {
+    "revision": "d250077db8ae9ec6c86d",
+    "url": "/infinitenaturalhealing/static/css/8.45f71c2e.chunk.css"
+  },
+  {
+    "revision": "d250077db8ae9ec6c86d",
+    "url": "/infinitenaturalhealing/static/js/8.1d8b6d25.chunk.js"
+  },
+  {
+    "revision": "8bc49cb110b287d6ce2b",
+    "url": "/infinitenaturalhealing/static/js/9.e65f8a0b.chunk.js"
+  },
+  {
+    "revision": "0b4bd7c5a9da7decbc92",
+    "url": "/infinitenaturalhealing/static/js/10.3e489349.chunk.js"
+  },
+  {
+    "revision": "85a72da759db5bc4c93f",
+    "url": "/infinitenaturalhealing/static/js/11.24fe757f.chunk.js"
+  },
+  {
+    "revision": "c8d8a195cf412cc69f1d",
+    "url": "/infinitenaturalhealing/static/js/12.bdbba2d5.chunk.js"
+  },
+  {
+    "revision": "907f96bbd57bbc368f9c",
+    "url": "/infinitenaturalhealing/static/js/13.a35731d7.chunk.js"
+  },
+  {
+    "revision": "a875ff72c5cdaa2da600",
+    "url": "/infinitenaturalhealing/static/js/14.0eda3629.chunk.js"
+  },
+  {
+    "revision": "c91e0de70b3f036b87e9",
+    "url": "/infinitenaturalhealing/static/js/15.a24f25fc.chunk.js"
+  },
+  {
+    "revision": "e47a00fa0e801cf9789e",
+    "url": "/infinitenaturalhealing/static/js/16.e8117ddc.chunk.js"
+  },
+  {
+    "revision": "f2d7c086960690fa0ea6",
+    "url": "/infinitenaturalhealing/static/js/17.a11b96a0.chunk.js"
+  },
+  {
+    "revision": "21443716a385e92119a2",
+    "url": "/infinitenaturalhealing/static/js/18.97cc63e1.chunk.js"
+  },
+  {
+    "revision": "fdbe29b25a297940493c",
+    "url": "/infinitenaturalhealing/static/js/19.f8bb602e.chunk.js"
+  },
+  {
+    "revision": "808a7be12c112763265a",
+    "url": "/infinitenaturalhealing/static/js/20.cc499fde.chunk.js"
+  },
+  {
+    "revision": "c5d85afc5b48473e2b0fefeea3766079",
+    "url": "/infinitenaturalhealing/index.html"
+  }
+];
